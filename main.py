bd56b9a1f0ecd0c070a7f49e040131aaf20de098
于斯同 <yusitong1999@foxmail.com>
2022-09-11 17:12:42 +0800

    feat: 支持抓取用户微博基本内容和附带内容的id

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import json
+import os
+import time
+import datetime
+
 import requests
+import pytz
 
 
 class MyEncoder(json.JSONEncoder):
     def default(self, obj):
+        if isinstance(obj, datetime.datetime):
+            return obj.strftime("%Y-%m-%d %H:%M:%S")
         return obj.__dict__
 
 
@@ -13,6 +20,11 @@ class CrawlerConfig:
         self.user_id_list: list[int] = data['user_id_list']
         self.user_agent: str = data['user_agent']
         self.cookie: str = data['cookie']
+        self.page_sleep_count: int = data['page_sleep_count']
+        self.page_sleep_duration: int = data['page_sleep_duration']
+        # 开始日期
+        self.since_date: datetime.datetime = pytz.UTC.localize(
+            datetime.datetime.strptime(data['since_data'], '%Y-%m-%d'))
 
 
 class User:
@@ -34,12 +46,47 @@ class User:
         self.statuses_count: int = user['statuses_count']
 
 
+class Mblog:
+    def __init__(self, mblog):
+        self.id: int = mblog['id']
+        self.mblog_id: str = mblog['mblogid']
+        self.text_raw: str = mblog['text_raw']
+        self.text: str = mblog['text']
+        # 指定、公开……
+        self.title: str = mblog['title']['text']
+        # 是否置顶
+        self.is_top: bool = self.title == '置顶'
+        # 发布地
+        self.region_name: str = mblog['region_name'] if 'region_name' in mblog else ''
+        # 发布时间
+        self.created_at: datetime.datetime = datetime.datetime.strptime(mblog['created_at'],
+                                                                        '%a %b %d %X %z %Y')
+
+        # 附带图片
+        self.has_picture: bool = len(mblog['pic_ids']) != 0
+        self.picture_id_list = list[str]()
+        self.picture_url_list = list[str]()
+        for picture_id in mblog['pic_ids']:
+            self.picture_id_list.append(picture_id)
+            self.picture_url_list.append(mblog['pic_infos'][picture_id]['largest']['url'])
+        # 附带文章
+        self.has_article: bool = 'url_struct' in mblog
+        if self.has_article:
+            self.article_url_list = list[str]()
+            for article in mblog['url_struct']:
+                self.article_url_list.append(article['long_url'])
+        # 是否转发
+        self.is_retweet: bool = 'retweeted_status' in mblog
+        self.retweet_id: int = mblog['retweeted_status']['id'] if self.is_retweet else 0
+
+
 class Crawler:
     def __init__(self, config: CrawlerConfig):
         self.config = config
         self.headers = {'user-agent': config.user_agent, 'cookie': config.cookie}
 
     def start(self):
+        os.makedirs('mblog', exist_ok=True)
         for user_id in self.config.user_id_list:
             # 抓取用户信息
             response = requests.get(f'https://weibo.com/ajax/profile/info?uid={user_id}', headers=self.headers)
@@ -48,6 +95,28 @@ class Crawler:
             user = User(data['data']['user'])
             with open(f'{user.id}.json', 'w', encoding='utf-8') as f:
                 json.dump(user, f, cls=MyEncoder, ensure_ascii=False)  #
+            # 抓取用户微博
+            i: int = 0
+            continue_flag: bool = True
+            while continue_flag:
+                i += 1
+                if i % self.config.page_sleep_count == 0:
+                    time.sleep(self.config.page_sleep_duration)
+
+                print(f'正在抓取第{i}页')
+                response = requests.get(f'https://weibo.com/ajax/statuses/mymblog?uid={user.id}&page={i}',
+                                        headers=self.headers)
+                data = json.loads(response.text)
+                if len(data['data']['list']) == 0:
+                    continue_flag = False
+
+                for data_mblog in data['data']['list']:
+                    mblog = Mblog(data_mblog)
+                    if mblog.created_at < self.config.since_date and not mblog.is_top:
+                        continue_flag = False
+                        break
+                    with open(f'mblog{os.sep}{mblog.id}.json', 'w', encoding='utf-8') as f:
+                        json.dump(mblog, f, cls=MyEncoder, ensure_ascii=False)
 
 
 if __name__ == '__main__':
